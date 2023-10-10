35ad066c99201144c19b993ae3ebba7ab2e2ed7c
RoJosGaRis <roberto.jgr03@gmail.com>
2023-10-10 17:31:31 -0600

    implementation of things

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
+const pg = require("pg");
+
 const app = express();
 const port = 3000;
 
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/users", (req, res) => {
+  console.log(process.env.DB_USER);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_NAME);
+  console.log(process.env.DB_PASS);
+  console.log(process.env.DB_PORT);
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) throw error;
+    res.send(results.rows);
+  });
+});
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
