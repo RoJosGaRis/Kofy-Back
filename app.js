054a9d09e3323a80d948197113075636241bd6c0
RoJosGaRis <roberto.jgr03@gmail.com>
2023-10-15 21:31:54 -0600

    hashing register and login works!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const pg = require("pg");
+// const pg = require("pg");
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
@@ -11,22 +11,22 @@ const prisma = new PrismaClient();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const Pool = pg.Pool;
+// const Pool = pg.Pool;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// });
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/logins", async (req, res) => {
-  const logins = await prisma.logins.findMany();
-  res.json(logins);
-});
+// app.get("/logins", async (req, res) => {
+//   const logins = await prisma.logins.findMany();
+//   res.json(logins);
+// });
 
 const userRouter = require("./routes/userSession");
 app.use("/user", userRouter);
--- routes/userSession.js
+++ routes/userSession.js
@@ -1,79 +1,107 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// const pg = require("pg");
+router.post("/register", async (req, res) => {
+  const { username, email, password, type } = req.body;
+  try {
+    bcrypt
+      .genSalt(Number(process.env.SALT_ROUNDS))
+      .then((salt) => {
+        return bcrypt.hash(password, salt);
+      })
+      .then(async (hash) => {
+        return (newUser = await prisma.logins.create({
+          data: {
+            username,
+            email,
+            password: hash,
+            type: Number(type),
+          },
+        }));
+      })
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  } catch (error) {
+    console.log(req.body);
+    res.status(400).json({ message: error.message });
+  }
+});
 
-// const Pool = pg.Pool;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT,
+  try {
+    const user = await prisma.logins.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error("Password does not match");
+    }
+
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// router.get("/logins", (req, res) => {
+//   pool.query("SELECT * FROM logins;", (error, results) => {
+//     if (error) {
+//       res.status(400).json({ message: error.message });
+//     }
+//     res.status(201).send(results.rows);
+//   });
 // });
 
-router.get("/loginsTest", async (req, res) => {
-  const logins = await prisma.logins.findMany();
-  res.json(logins);
-});
+// router.put("/updateLogin", (req, res) => {
+//   pool.query(
+//     "UPDATE logins SET username = $2, email = $3, password = $4, type = $5 WHERE id = $1 RETURNING *;",
+//     [
+//       req.body.id,
+//       req.body.username,
+//       req.body.email,
+//       req.body.password,
+//       req.body.type,
+//     ],
+//     (error, results) => {
+//       if (error) {
+//         res.status(400).json({ message: error.message });
+//       } else {
+//         res.status(201).send(results.rows);
+//       }
+//     }
+//   );
+// });
 
-router.post("/register", async (req, res) => {
-  pool.query(
-    "INSERT INTO logins (username, email, password, type) VALUES ($1, $2, $3, $4) RETURNING *;",
-    [req.body.username, req.body.email, req.body.password, req.body.type],
-    (error, results) => {
-      if (error) {
-        res.status(400).json({ message: error.message });
-      }
-      res.status(201).send(results.rows);
-    }
-  );
-});
-
-router.get("/logins", (req, res) => {
-  pool.query("SELECT * FROM logins;", (error, results) => {
-    if (error) {
-      res.status(400).json({ message: error.message });
-    }
-    res.status(201).send(results.rows);
-  });
-});
-
-router.put("/updateLogin", (req, res) => {
-  pool.query(
-    "UPDATE logins SET username = $2, email = $3, password = $4, type = $5 WHERE id = $1 RETURNING *;",
-    [
-      req.body.id,
-      req.body.username,
-      req.body.email,
-      req.body.password,
-      req.body.type,
-    ],
-    (error, results) => {
-      if (error) {
-        res.status(400).json({ message: error.message });
-      } else {
-        res.status(201).send(results.rows);
-      }
-    }
-  );
-});
-
-router.delete("/deleteLogin", (req, res) => {
-  pool.query(
-    "DELETE FROM logins WHERE id = $1 RETURNING *",
-    [req.body.id],
-    (error, results) => {
-      if (error) {
-        res.status(400).json({ message: error.message });
-      } else {
-        res.status(201).send(results.rows);
-      }
-    }
-  );
-});
+// router.delete("/deleteLogin", (req, res) => {
+//   pool.query(
+//     "DELETE FROM logins WHERE id = $1 RETURNING *",
+//     [req.body.id],
+//     (error, results) => {
+//       if (error) {
+//         res.status(400).json({ message: error.message });
+//       } else {
+//         res.status(201).send(results.rows);
+//       }
+//     }
+//   );
+// });
 
 module.exports = router;
