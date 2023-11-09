36a50dfe55cbfec8f3eacf0e008aa96d2b5bbc29
RoJosGaRis <roberto.jgr03@gmail.com>
2023-11-09 15:18:56 -0600

    Add setProfile route to create new user profile

--- routes/profile.js
+++ routes/profile.js
@@ -27,5 +27,60 @@ router.post("/getProfile", validateToken, async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+router.post("/setProfile", validateToken, async (req, res) => {
+  const {
+    userId,
+    names,
+    last_names,
+    birthday,
+    gender,
+    profile_picture,
+    blood_type,
+    height,
+    weight,
+    allergies,
+    diseases,
+  } = req.body;
+
+  let existingUser;
+  try {
+    const oldUser = await prisma.profiles.findUnique({
+      where: {
+        login_id: parseInt(userId),
+      },
+    });
+
+    if (oldUser) {
+      throw new Error("User already exists");
+    } else {
+      // var dateString = "2012-05-15";
+      var year = birthday.split("-")[0];
+      var month = birthday.split("-")[1];
+      var day = birthday.split("-")[2];
+
+      var birthDate = new Date(year, month - 1, day);
+      // var currentDate = new Date();
+
+      const newUser = await prisma.profiles.create({
+        data: {
+          login_id: parseInt(userId),
+          names,
+          last_names,
+          birthday: birthDate,
+          gender,
+          profile_picture: parseInt(profile_picture),
+          blood_type,
+          height: parseInt(height),
+          weight: parseFloat(weight),
+          allergies,
+          diseases,
+        },
+      });
+      res.status(201).send(newUser);
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
