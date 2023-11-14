ed7144e3bcebf9ba3b677b8ad756404b71329312
RoJosGaRis <roberto.jgr03@gmail.com>
2023-11-13 22:05:36 -0600

    Refactor getCardCollections route to include icon URLs and add new post route for retrieving a single collection by ID.

--- routes/learning.js
+++ routes/learning.js
@@ -7,14 +7,26 @@ const validateToken = require("../helper/validateToken");
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get("/getCardCollections", (req, res) => {
-  const collections = prisma.card_collections.findMany({
-    select: {
-      name: true,
-      image: true,
-    },
+router.get("/getCardCollections", async (req, res) => {
+  const collections = await prisma.card_collections.findMany();
+  let fullUrl = req.protocol + "://" + req.get("host") + "/images";
+
+  collections.forEach((element) => {
+    currIcon = element.icon;
+    element.icon = fullUrl + element.icon;
+    // console.log(fullUrl + element.icon);
   });
 
+  res.send(collections);
+});
+router.post("/getCardCollections", async (req, res) => {
+  id = req.body.id;
+
+  const collections = await prisma.card_collections.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
   res.send(collections);
 });
 
