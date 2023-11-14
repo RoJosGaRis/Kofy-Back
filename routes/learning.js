dcc2ab27af4ada056d8bcddad36dc2b6b6a07d08
RoJosGaRis <roberto.jgr03@gmail.com>
2023-11-14 14:30:27 -0600

    Add function to translate card elements and update response format

--- routes/learning.js
+++ routes/learning.js
@@ -7,6 +7,19 @@ const validateToken = require("../helper/validateToken");
 const prisma = new PrismaClient();
 const router = express.Router();
 
+const translateCard = (element, fullUrl) => {
+  let newElement = {
+    id: element.id,
+    index: element.index,
+    content: element.content,
+    imageLink: fullUrl + element.image_link,
+    isVideo: element.is_video,
+    videoLink: element.video_link,
+  };
+
+  return newElement;
+};
+
 router.get("/getCardCollections", async (req, res) => {
   const collections = await prisma.card_collections.findMany();
   let fullUrl = req.protocol + "://" + req.get("host") + "/images";
@@ -37,13 +50,12 @@ router.post("/getCardCollections", async (req, res) => {
     },
   });
 
-  cards.forEach((element) => {
-    currIcon = element.icon;
-    element.image_link = fullUrl + element.image_link;
+  cards.forEach((element, index) => {
+    cards[index] = translateCard(element, fullUrl);
     // console.log(fullUrl + element.icon);
   });
 
-  res.send(cards);
+  res.json(cards);
 });
 
 module.exports = router;
